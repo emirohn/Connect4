45890b3bee88fb26c15c5dfbf00edf142e175237
emirohn <51095479+emirohn@users.noreply.github.com>
2020-04-17 16:49:36 -0700

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Runner {
     public static void main (String[] args){
 
+        System.out.println("Note: there are 8 columns, numbered 0 through 7");
+
         Scanner sc = new Scanner(System.in);
         Human h = new Human("Emi", "X");
         Computer c = new Computer("O");
@@ -19,6 +21,12 @@ public class Runner {
                 if(sc.hasNextInt()){
                     moveCol = sc.nextInt();
                     legalCol = true;
+
+                    if(moveCol >=7 || moveCol <= 0){
+                        legalCol = false;
+                        System.out.println("Enger a legal column!");
+                    }
+
                 }else{
                     System.out.println("Enger a legal column!");
                     sc.next();
@@ -46,7 +54,7 @@ public class Runner {
                     if(b.board[i][moveCol].equals("  [   ]  ")) {
                         cMove[0] = i;
                     }
-                //see if move is already occupied
+                    //see if move is already occupied
                 }
                 if(b.board[cMove[0]][cMove[1]].equals("  [   ]  ")){
                     b.makeMove(cMove[0],cMove[1], c.token);
